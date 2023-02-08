385f46de85aa90d3bd43b9eaa93e0650416c0727
Carlos Bueno <carlosbueno.contato@gmail.com>
2023-02-08 11:03:35 -0300

    back to basics

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from datetime import datetime
 import os
 import json
@@ -16,21 +16,20 @@ def index():
     webhook_invocation_id = request.headers.get('X-Zendesk-Webhook-Invocation-Id')
     user_agent = request.headers.get('User-Agent')
 
-    body = {
+
+    print(f'datetime_from_flask_server: {date_time}\nticket_id: {ticket_id}\naccount_id: {account_id}\nwebhook_id: {webhook_id}\nwebhook_invocation_id : {webhook_invocation_id}\nuser_agent: {user_agent}')
+
+    
+
+
+    return jsonify({
         "datetime_from_flask_server": date_time,
         "ticket_id": ticket_id,
         "account_id": account_id,
         "webhook_id": webhook_id,
         "webhook_invocation_id" : webhook_invocation_id,
         "user_agent": user_agent
-    }
-
-    print(f'datetime_from_flask_server: {date_time}\nticket_id: {ticket_id}\naccount_id: {account_id}\nwebhook_id: {webhook_id}\nwebhook_invocation_id : {webhook_invocation_id}\nuser_agent: {user_agent}')
-
-    json_object = json.dumps(body, indent = 4)
-
-    print(body)
-    return json_object
+    })
 
 
 
