281eb530b7afadb9eddb92c2bdbb4f31b5be4cdf
Carlos Bueno <carlosbueno.contato@gmail.com>
2023-02-08 10:53:41 -0300

    datetime things

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     dt = datetime.now()
+    date_time = dt.strftime("%m/%d/%Y, %H:%M:%S")
     ticket_id = request.args.get('test')
     account_id = request.headers.get('X-Zendesk-Account-Id')
     webhook_id = request.headers.get('X-Zendesk-Webhook-Id')
@@ -16,7 +17,7 @@ def index():
     user_agent = request.headers.get('User-Agent')
 
     body = {
-        "datetime_from_flask_server": dt,
+        "datetime_from_flask_server": date_time,
         "ticket_id": ticket_id,
         "account_id": account_id,
         "webhook_id": webhook_id,
@@ -24,7 +25,7 @@ def index():
         "user_agent": user_agent
     }
 
-    print(f'datetime_from_flask_server: {dt}\nticket_id: {ticket_id}\naccount_id: {account_id}\nwebhook_id: {webhook_id}\nwebhook_invocation_id : {webhook_invocation_id}\nuser_agent: {user_agent}')
+    print(f'datetime_from_flask_server: {date_time}\nticket_id: {ticket_id}\naccount_id: {account_id}\nwebhook_id: {webhook_id}\nwebhook_invocation_id : {webhook_invocation_id}\nuser_agent: {user_agent}')
 
     json_object = json.dumps(body, indent = 4)
 
