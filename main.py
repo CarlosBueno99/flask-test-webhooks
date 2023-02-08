3c88bd50e2149d184e1dfefaa31d1b4c7b16e9e9
Carlos Bueno <carlosbueno.contato@gmail.com>
2023-02-08 11:15:39 -0300

    creating the URLs

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ def index():
     webhook_id = request.headers.get('X-Zendesk-Webhook-Id')
     webhook_invocation_id = request.headers.get('X-Zendesk-Webhook-Invocation-Id')
     user_agent = request.headers.get('User-Agent')
+    internal_zendesk_endpoint_api = f'https://[you-subdomain].zendesk.com/api/v2/webhooks/{webhook_id}/invocations/{webhook_invocation_id}/attempts'
+    invocation_ui_URL = f'https://[you-subdomain].zendesk.com/admin/apps-integrations/webhooks/webhooks/{webhook_id}/activity/{webhook_invocation_id}'
 
     print('=-=-=-=-=-=-=-=-=-=-=-= New request =-=-=-=-=-=-=-=-=-=-=-=')
-    print(f'datetime_from_flask_server: {date_time}\nticket_id: {ticket_id}\naccount_id: {account_id}\nwebhook_id: {webhook_id}\nwebhook_invocation_id : {webhook_invocation_id}\nuser_agent: {user_agent}')
+    print(f'datetime_from_flask_server: {date_time}\nticket_id: {ticket_id}\naccount_id: {account_id}\nwebhook_id: {webhook_id}\nwebhook_invocation_id : {webhook_invocation_id}\nuser_agent: {user_agent}\nui_endpoint: {invocation_ui_URL}\napi_endpoint: {internal_zendesk_endpoint_api}')
 
     
 
@@ -28,7 +30,9 @@ def index():
         "account_id": account_id,
         "webhook_id": webhook_id,
         "webhook_invocation_id" : webhook_invocation_id,
-        "user_agent": user_agent
+        "user_agent": user_agent,
+        "ui_endpoint": invocation_ui_URL,
+        "api_endpoint": internal_zendesk_endpoint_api
     })
 
 
